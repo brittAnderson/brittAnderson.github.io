117cc822cb6f6abbaf4c75fdf541eb41967fa372
Britt Anderson <britt@uwaterloo.ca>
2021-03-29 12:14:15 -0400

    revising site for github page method

---
+++ site.hs
@@ -0,0 +1,91 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+--------------------------------------------------------------------------------
+mkMyCfg :: IO Configuration
+mkMyCfg = return (defaultConfiguration {destinationDirectory = "docs"})
+
+main :: IO ()
+main = do 
+mycfg <- mkMyCfg 
+hakyllWith  mycfg $ do
+match "images/*" $ do
+route   idRoute
+compile copyFileCompiler
+
+match "css/*" $ do
+route   idRoute
+compile compressCssCompiler
+
+match (fromList ["about.rst", "contact.markdown"]) $ do
+route   $ setExtension "html"
+compile $ pandocCompiler
+    >>= loadAndApplyTemplate "templates/default.html" defaultContext
+    >>= relativizeUrls
+
+match "posts/*" $ do
+route $ setExtension "html"
+compile $ pandocCompiler
+    >>= loadAndApplyTemplate "templates/post.html"    postCtx
+    >>= saveSnapshot "content"
+    >>= loadAndApplyTemplate "templates/default.html" postCtx
+    >>= relativizeUrls
+
+create ["rss.xml"] $ do
+   route idRoute
+   compile $ do
+	let feedCtx = postCtx `mappend`
+	     bodyField "description" 
+	posts <- fmap (take 10) . recentFirst =<< loadAllSnapshots "posts/*" "content"
+	renderRss myFeedConfiguration feedCtx posts
+
+create ["archive.html"] $ do
+route idRoute
+compile $ do
+    posts <- recentFirst =<< loadAll "posts/*"
+    let archiveCtx =
+	    listField "posts" postCtx (return posts) `mappend`
+	    constField "title" "Archives"            `mappend`
+	    defaultContext
+
+    makeItem ""
+	>>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+	>>= loadAndApplyTemplate "templates/default.html" archiveCtx
+	>>= relativizeUrls
+
+
+match "index.html" $ do
+route idRoute
+compile $ do
+    posts <- recentFirst =<< loadAll "posts/*"
+    let indexCtx =
+	    listField "posts" postCtx (return posts) `mappend`
+	    constField "title" "Home"                `mappend`
+	    defaultContext
+
+    getResourceBody
+	>>= applyAsTemplate indexCtx
+	>>= loadAndApplyTemplate "templates/default.html" indexCtx
+	>>= relativizeUrls
+
+match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+dateField "date" "%B %e, %Y" `mappend`
+defaultContext
+
+myFeedConfiguration :: FeedConfiguration
+myFeedConfiguration = FeedConfiguration
+{ feedTitle       = "Britt Anderson´s Blog"
+, feedDescription = "Little Random Thought Droplets"
+, feedAuthorName  = "Britt Anderson"
+, feedAuthorEmail = "britt@uwaterloo.ca"
+, feedRoot        = "https://brittanderson.github.io"
+}
+
